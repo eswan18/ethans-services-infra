bfc96fc80d23c970f2420dfc176baaad5c592ece
Ethan Swan <ethanpswan@gmail.com>
2026-02-06 08:11:28 -0600

    Set up clusters.

---
+++ __main__.py
@@ -0,0 +1,294 @@
+"""Infrastructure for Ethan's Services in GCP"""
+
+import pulumi
+from pulumi import ResourceOptions
+from pulumi_gcp import container, serviceaccount, projects, artifactregistry
+
+# Configuration
+project = "ethans-services"
+region = "us-central1"
+zone = "us-central1-a"
+
+# Artifact Registry repository
+container_registry = artifactregistry.Repository(
+    "containers",
+    description="Container images",
+    format="DOCKER",
+    location=region,
+    project=project,
+    repository_id="containers",
+    opts=pulumi.ResourceOptions(protect=True),
+)
+
+# GKE Cluster
+main_cluster = container.Cluster("main-cluster",
+    addons_config={
+        "gce_persistent_disk_csi_driver_config": {
+            "enabled": True,
+        },
+        "network_policy_config": {
+            "disabled": True,
+        },
+    },
+    anonymous_authentication_config={
+        "mode": "ENABLED",
+    },
+    cluster_ipv4_cidr="10.36.0.0/14",
+    cluster_telemetry={
+        "type": "ENABLED",
+    },
+    control_plane_endpoints_config={
+        "dns_endpoint_config": {
+            "endpoint": "gke-3fd139f806604cd19549a21e2dd49874e01e-754418346661.us-central1-a.gke.goog",
+        },
+        "ip_endpoints_config": {
+            "enabled": True,
+        },
+    },
+    database_encryption={
+        "state": "DECRYPTED",
+    },
+    default_max_pods_per_node=110,
+    location="us-central1-a",
+    logging_config={
+        "enable_components": [
+            "SYSTEM_COMPONENTS",
+            "WORKLOADS",
+        ],
+    },
+    master_auth={
+        "client_certificate_config": {
+            "issue_client_certificate": False,
+        },
+    },
+    monitoring_config={
+        "advanced_datapath_observability_config": {
+            "enable_metrics": False,
+            "enable_relay": False,
+        },
+        "enable_components": [
+            "SYSTEM_COMPONENTS",
+            "STORAGE",
+            "HPA",
+            "POD",
+            "DAEMONSET",
+            "DEPLOYMENT",
+            "STATEFULSET",
+            "JOBSET",
+            "CADVISOR",
+            "KUBELET",
+            "DCGM",
+        ],
+        "managed_prometheus": {
+            "enabled": True,
+        },
+    },
+    name="main-cluster",
+    network="projects/ethans-services/global/networks/default",
+    network_policy={
+        "enabled": False,
+        "provider": "PROVIDER_UNSPECIFIED",
+    },
+    networking_mode="VPC_NATIVE",
+    node_config={
+        "boot_disk": {
+            "disk_type": "pd-balanced",
+            "size_gb": 20,
+        },
+        "disk_size_gb": 20,
+        "disk_type": "pd-balanced",
+        "image_type": "COS_CONTAINERD",
+        "kubelet_config": {
+            "insecure_kubelet_readonly_port_enabled": "FALSE",
+            "max_parallel_image_pulls": 2,
+        },
+        "logging_variant": "DEFAULT",
+        "machine_type": "e2-medium",
+        "metadata": {
+            "disable-legacy-endpoints": "true",
+        },
+        "oauth_scopes": [
+            "https://www.googleapis.com/auth/devstorage.read_only",
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring",
+            "https://www.googleapis.com/auth/service.management.readonly",
+            "https://www.googleapis.com/auth/servicecontrol",
+            "https://www.googleapis.com/auth/trace.append",
+        ],
+        "resource_labels": {
+            "goog-gke-node-pool-provisioning-model": "spot",
+        },
+        "service_account": "default",
+        "spot": True,
+        "workload_metadata_config": {
+            "mode": "GKE_METADATA",
+        },
+    },
+    node_pool_auto_config={
+        "node_kubelet_config": {
+            "insecure_kubelet_readonly_port_enabled": "FALSE",
+        },
+    },
+    node_pool_defaults={
+        "node_config_defaults": {
+            "insecure_kubelet_readonly_port_enabled": "FALSE",
+            "logging_variant": "DEFAULT",
+        },
+    },
+    node_pools=[
+        {
+            "initial_node_count": 1,
+            "max_pods_per_node": 110,
+            "name": "spot-pool-medium",
+            "network_config": {
+                "pod_ipv4_cidr_block": "10.36.0.0/14",
+                "pod_range": "gke-main-cluster-pods-3fd139f8",
+            },
+            "node_config": {
+                "boot_disk": {
+                    "disk_type": "pd-balanced",
+                    "size_gb": 20,
+                },
+                "disk_size_gb": 20,
+                "disk_type": "pd-balanced",
+                "image_type": "COS_CONTAINERD",
+                "kubelet_config": {
+                    "insecure_kubelet_readonly_port_enabled": "FALSE",
+                    "max_parallel_image_pulls": 2,
+                },
+                "logging_variant": "DEFAULT",
+                "machine_type": "e2-medium",
+                "metadata": {
+                    "disable-legacy-endpoints": "true",
+                },
+                "oauth_scopes": [
+                    "https://www.googleapis.com/auth/devstorage.read_only",
+                    "https://www.googleapis.com/auth/logging.write",
+                    "https://www.googleapis.com/auth/monitoring",
+                    "https://www.googleapis.com/auth/service.management.readonly",
+                    "https://www.googleapis.com/auth/servicecontrol",
+                    "https://www.googleapis.com/auth/trace.append",
+                ],
+                "resource_labels": {
+                    "goog-gke-node-pool-provisioning-model": "spot",
+                },
+                "service_account": "default",
+                "spot": True,
+                "workload_metadata_config": {
+                    "mode": "GKE_METADATA",
+                },
+            },
+            "node_count": 1,
+            "node_locations": ["us-central1-a"],
+            "upgrade_settings": {
+                "max_surge": 1,
+            },
+            "version": "1.33.5-gke.2118001",
+        },
+        {
+            "initial_node_count": 1,
+            "max_pods_per_node": 110,
+            "name": "default-pool-std2",
+            "network_config": {
+                "pod_ipv4_cidr_block": "10.36.0.0/14",
+                "pod_range": "gke-main-cluster-pods-3fd139f8",
+            },
+            "node_config": {
+                "boot_disk": {
+                    "disk_type": "pd-balanced",
+                    "size_gb": 100,
+                },
+                "disk_size_gb": 100,
+                "disk_type": "pd-balanced",
+                "image_type": "COS_CONTAINERD",
+                "kubelet_config": {
+                    "insecure_kubelet_readonly_port_enabled": "FALSE",
+                    "max_parallel_image_pulls": 2,
+                },
+                "logging_variant": "DEFAULT",
+                "machine_type": "e2-standard-2",
+                "metadata": {
+                    "disable-legacy-endpoints": "true",
+                },
+                "oauth_scopes": [
+                    "https://www.googleapis.com/auth/devstorage.read_only",
+                    "https://www.googleapis.com/auth/logging.write",
+                    "https://www.googleapis.com/auth/monitoring",
+                    "https://www.googleapis.com/auth/service.management.readonly",
+                    "https://www.googleapis.com/auth/servicecontrol",
+                    "https://www.googleapis.com/auth/trace.append",
+                ],
+                "resource_labels": {
+                    "goog-gke-node-pool-provisioning-model": "on-demand",
+                },
+                "service_account": "default",
+                "workload_metadata_config": {
+                    "mode": "GKE_METADATA",
+                },
+            },
+            "node_count": 1,
+            "node_locations": ["us-central1-a"],
+            "upgrade_settings": {
+                "max_surge": 1,
+            },
+            "version": "1.33.5-gke.2118001",
+        },
+    ],
+    node_version="1.33.5-gke.2118001",
+    notification_config={
+        "pubsub": {
+            "enabled": False,
+        },
+    },
+    pod_autoscaling={
+        "hpa_profile": "PERFORMANCE",
+    },
+    pod_security_policy_config={
+        "enabled": False,
+    },
+    private_cluster_config={
+        "master_global_access_config": {
+            "enabled": False,
+        },
+    },
+    project="ethans-services",
+    protect_config={
+        "workload_config": {
+            "audit_mode": "BASIC",
+        },
+        "workload_vulnerability_mode": "WORKLOAD_VULNERABILITY_MODE_UNSPECIFIED",
+    },
+    rbac_binding_config={
+        "enable_insecure_binding_system_authenticated": True,
+        "enable_insecure_binding_system_unauthenticated": True,
+    },
+    release_channel={
+        "channel": "REGULAR",
+    },
+    secret_manager_config={
+        "enabled": False,
+    },
+    secret_sync_config={
+        "enabled": False,
+    },
+    security_posture_config={
+        "mode": "BASIC",
+        "vulnerability_mode": "VULNERABILITY_MODE_UNSPECIFIED",
+    },
+    service_external_ips_config={
+        "enabled": False,
+    },
+    subnetwork="projects/ethans-services/regions/us-central1/subnetworks/default",
+    workload_identity_config={
+        "workload_pool": "ethans-services.svc.id.goog",
+    },
+    opts = pulumi.ResourceOptions(protect=True),
+)
+
+
+# Export cluster info
+pulumi.export("cluster_name", main_cluster.name)
+pulumi.export("cluster_endpoint", main_cluster.endpoint)
+pulumi.export("registry_url", container_registry.id.apply(
+    lambda id: f"{region}-docker.pkg.dev/{project}/containers"
+))
